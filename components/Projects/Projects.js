bb9829c537b19d19222b1f420644979444789e51
Vajresh <vajresh005@example.com>
2021-05-31 01:02:59 +0530

    added functionality to select tags and minor changes in the card and tags style.

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 import styles from "./Projects.module.scss";
 
 function Card(props) {
   const { title, imagePath, tags, description, sourceCodePath, liveDemoPath } =
     props;
+
   return (
     <section className={styles.card}>
       {/* add project status like complete, in progress, not started */}
@@ -29,26 +31,50 @@ function Card(props) {
   );
 }
 
+function useFilterProjects(tagsList = []) {
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [tags, setTags] = useState(tagsList);
+
+  function handleClick(e, index) {
+    console.log(e.target, index);
+
+    const allSelectedTags = [...selectedTags];
+    const newSelectedTag = tags[index];
+
+    allSelectedTags.push(newSelectedTag);
+    const filterSelectedTag = tags.filter((tag) => (tag !== newSelectedTag));
+
+    setTags(filterSelectedTag);
+    setSelectedTags(allSelectedTags);
+  }
+
+  return [tags, selectedTags || [], handleClick];
+}
+
+
+const tagLists = ["HTML", "CSS", "JavaScript", "React Js", "Node JS"];
+const projects = [
+  {
+    title: "Media Tracker",
+    imagePath: "/assets/media-tracker.PNG",
+    tags: ["React Js", "JavaScript", "HTML"],
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Minus quis sed, iure inventore nulla aspernatur alias dolorem nemo asperiores, tempora, labore assumenda corporis doloribus. Modi culpa dolor beatae vero quibusdam!",
+    sourceCodePath: "#",
+    liveDemoPath: "#",
+  },
+  {
+    title: "Project 2",
+    imagePath: "/assets/chat.svg",
+    tags: ["React Js", "JavaScript", "HTML"],
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    sourceCodePath: "#",
+    liveDemoPath: "#",
+  },
+];
+
 export default function Projects() {
-  const tags = ["HTML", "CSS", "JavaScript", "React Js"];
-  const projects = [
-    {
-      title: "Media Tracker",
-      imagePath: "/assets/media-tracker.PNG",
-      tags: ["React Js", "JavaScript", "HTML"],
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      sourceCodePath: "#",
-      liveDemoPath: "#",
-    },
-    {
-      title: "Project 2",
-      imagePath: "/assets/chat.svg",
-      tags: ["React Js", "JavaScript", "HTML"],
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      sourceCodePath: "#",
-      liveDemoPath: "#",
-    },
-  ];
+  const [tags, selectedTags, handleClick] = useFilterProjects(tagLists);
+
 
   return (
     <div className={styles.projects}>
@@ -60,26 +86,29 @@ export default function Projects() {
             Search
           </label>
         </div>
+
         <div className={styles.tags}>
-          {tags.map((tag) => {
+          {selectedTags.map((tag) => {
             return <span key={tag}>{tag}</span>;
+          })}
+          {tags.map((tag, index) => {
+            return <span key={tag} onClick={(e) => handleClick(e, index)}>{tag}</span>;
           })}
         </div>
       </section>
+
       <section className={styles.cardContainer}>
-        <h2>Total Projects (0)</h2>
+        <h2>Total Projects ({projects.length})</h2>
+
         {projects.map((project) => {
           return (
             <Card
               title={project.title}
               imagePath={project.imagePath}
-              tags={["React Js", "JavaScript", "HTML"]}
-              description="
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Consequatur iure cupiditate odio!
-                Explicabo, perspiciatis praesentium!"
-              sourceCodePath="#"
-              liveDemoPath="#"
+              tags={project.tags}
+              description={project.description}
+              sourceCodePath={project.sourceCodePath}
+              liveDemoPath={project.liveDemoPath}
               key={project.title}
             />
           );
