937a7d5865fa789defb1826f75b552f589c6facc
Vajresh <vajresh005@example.com>
2021-06-14 23:30:57 +0530

    added html file and google analytics tag in head for google search console verification

--- components/Layout/MetaData/MetaData.js
+++ components/Layout/MetaData/MetaData.js
@@ -9,6 +9,23 @@ export default function MetaData({ title, favicon = "/favicon.ico" }) {
       <meta name="description" content="Vajresh Personal Website" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
 
       <link rel="icon" href={favicon} />
       
